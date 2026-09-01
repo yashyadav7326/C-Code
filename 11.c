f091abd3c128d6801be4e141c7d54169ff4e90ac
yashyadav7326 <yashyadav7326@gmail.com>
2026-09-01 13:01:00 +0530

    update

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a= 81;
+    int b = 83;
+    int c = 92;
+    int d = 72;
+    int e = 85;
+    printf("account = %d\n",a);
+    printf("eco = %d\n",b);
+    printf("business= %d\n",c);
+    printf("english = %d\n",d);
+    printf("hindi = %d\n",e);
+
+    printf("persenateg = %d",(a+b+c+d+e/5));
+    return 0;
+
+
+
+}
