ee05c7b5f2e52b54e4525cb3ca5337914c6e08fb
yashyadav7326 <yashyadav7326@gmail.com>
2026-09-01 12:21:20 +0530

    update

---
+++ 7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    float length , breadth,area;
+    printf("enter length : ");
+    scanf("%f",&length);
+
+    printf("enter breadth : ");
+    scanf("%f",&breadth);
+
+    area = length*breadth;
+    printf("area %f",area);
+    return 0;
+
+    
+}
